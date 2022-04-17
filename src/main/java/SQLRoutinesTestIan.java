694ab85cf4aef5e681ff94f6c92bb5027641255d
Ian W <iwilson001@csbsju.edu>
2022-04-17 15:27:19 -0500

    Inital Commit of JSP project

---
+++ src/main/java/SQLRoutinesTestIan.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+//import org.junit.*;
+import oracle.jdbc.*;
+/**
+ * Auto Generated Java Class.
+ */
+public class SQLRoutinesTestIan {
+  public static void main(String[] args) {
+        SQLRoutinesIan s = new SQLRoutinesIan();
+  s.viewTeamRoster("THE JETS");
+    }
+  
+  
+  /* ADD YOUR CODE HERE */
+  
+}
