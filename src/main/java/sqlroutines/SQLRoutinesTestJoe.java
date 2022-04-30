2aaf40efdc8de746709f931b42162e3b77140cf7
JSEBENALE001 <jsebenale001@vplincsci050.ad.csbsju.edu>
2022-04-30 14:56:28 -0500

    login started

---
+++ src/main/java/sqlroutines/SQLRoutinesTestJoe.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+//import org.junit.*;
+import oracle.jdbc.*;
+/**
+ * Auto Generated Java Class.
+ */
+public class SQLRoutinesTestJoe {
+  public static void main(String[] args) 
+   {
+        SQLRoutinesJoe s = new SQLRoutinesJoe();
+        s.loginPlayer("LJONES001@csbsju.edu","1234");
+        System.out.print(s.loginPlayer("LJONES001@csbsju.edu","1234"));
+    }
+  
+  
+  /* ADD YOUR CODE HERE */
+  
+}
